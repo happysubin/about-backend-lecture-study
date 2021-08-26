f844cf551bf83e76dd44279c7507d8f8c31e70c9
happysubin <76802855+happysubin@users.noreply.github.com>
2021-08-27 00:14:39 +0900

    Initial commit

---
+++ src/test/java/hello/hello/world/spring/HelloWorldSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.hello.world.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
