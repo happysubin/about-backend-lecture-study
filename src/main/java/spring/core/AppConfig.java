e45901ccd2d97ffe235dd3cd9bf03eb30d2da207
happysubin <asb0711@gmail.com>
2021-12-17 16:18:47 +0900

    AppConfig Refactoring

--- src/main/java/spring/core/AppConfig.java
+++ src/main/java/spring/core/AppConfig.java
@@ -1,23 +1,36 @@
 package spring.core;
 
+import spring.core.discount.DiscountPolicy;
+import spring.core.discount.FixDiscountPolicy;
 import spring.core.discount.RateDiscountPolicy;
+import spring.core.member.MemberRepository;
 import spring.core.member.MemberService;
 import spring.core.member.MemberServiceImpl;
 import spring.core.member.MemoryMemberRepository;
 import spring.core.order.OrderService;
 import spring.core.order.OrderServiceImpl;
 
-
-
 public class AppConfig {
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService(){
-        return new OrderServiceImpl(new RateDiscountPolicy(),new MemoryMemberRepository());
+        return new OrderServiceImpl(discountPolicy(),memberRepository());
+    }
+    
+    public DiscountPolicy discountPolicy(){
+        return new FixDiscountPolicy();
     }
 }
 
 //실제 동작에 필요한 구현 객체는 모조리 여기서 생성.
 //AppConfig 는 생성한 객체 인스턴스의 참조를 생성자를 통해서 주입한다.
+
+//MemoryRepository 중복 제거
+//리팩토링 시 큰 장점. 역할이 잘 드러난다. 메소드 명을 보면 역할 파악!
+//나중에 memberRepository를 바꾸면 저 코드만 바꾸면 된다. 수정에 용이
