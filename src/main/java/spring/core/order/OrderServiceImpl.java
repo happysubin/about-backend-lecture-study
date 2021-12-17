d72d3fe55da67ee4953bd23d368cadec1bb9fbcb
happysubin <asb0711@gmail.com>
2021-12-17 15:23:56 +0900

    새로운 할인 정책 적용과 문제점
    userServiceImpl 코드를 바꾸므로 OCP 위반, DiscountPolicy 추상화에만
    의존하지 않고 구현 객체애도 의존하므로 DIP 위반

--- src/main/java/spring/core/order/OrderServiceImpl.java
+++ src/main/java/spring/core/order/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package spring.core.order;
 
 import spring.core.discount.DiscountPolicy;
 import spring.core.discount.FixDiscountPolicy;
+import spring.core.discount.RateDiscountPolicy;
 import spring.core.member.Member;
 import spring.core.member.MemberRepository;
 import spring.core.member.MemoryMemberRepository;
@@ -9,8 +10,11 @@ import spring.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository=new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy=new FixDiscountPolicy();
+    //private final DiscountPolicy discountPolicy=new FixDiscountPolicy();
+    private final DiscountPolicy discountPolicy=new RateDiscountPolicy();
 
+
+    //private DiscountPolicy discountPolicy; 이 코드가 바로 추상화에 의존하는 코드!!! 그런데 실행은 당연히 안돼
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member=memberRepository.findById(memberId);
