81e7193952dd2eef2b49b4407731b2a716416ffd
happysubin <asb0711@gmail.com>
2022-01-03 16:02:55 +0900

    Http request - Post html form

--- src/main/java/hello/servlet/basic/request/RequestParamServlet.java
+++ src/main/java/hello/servlet/basic/request/RequestParamServlet.java
@@ -53,3 +53,17 @@ public class RequestParamServlet extends HttpServlet {
         res.getWriter().write("ok");
     }
 }
+
+
+/**
+ * application/x-www-form-urlencoded 형식은 앞서 GET에서 살펴본 쿼리 파라미터 형식과 같다.
+ * 따라서 쿼리 파라미터 조회 메서드를 그대로 사용하면 된다.
+ * 클라이언트(웹 브라우저) 입장에서는 두 방식에 차이가 있지만, 서버 입장에서는 둘의 형식이 동일하므로,
+ * request.getParameter() 로 편리하게 구분없이 조회할 수 있다.
+ * 정리하면 request.getParameter() 는 GET URL 쿼리 파라미터 형식도 지원하고, POST HTML Form 형식도 둘 다 지원한다.
+ *
+ * POST HTML Form 형식으로 데이터를 전달하면 HTTP 메시지 바디에 해당 데이터를 포함해서 보내기
+ * 때문에 바디에 포함된 데이터가 어떤 형식인지 content-type을 꼭 지정해야 한다.
+ *
+ * 쿼리파라미터 ! 폼 형식 둘다 똑같이 지원!!!!
+ */
