432cbe42e274f2d791d74b572ca2db9173e9bea1
happysubin <asb0711@gmail.com>
2022-01-04 16:51:11 +0900

    Model 도입 version 3
    ModelView 를 도입 controller가 이제  servlet에 종속적이지 않다.
    파일이름 중복도 줄임.

--- src/main/java/hello/servlet/web/frontcontroller/MyView.java
+++ src/main/java/hello/servlet/web/frontcontroller/MyView.java
@@ -6,6 +6,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
 
 public class MyView {
     private String viewPath;
@@ -18,6 +19,15 @@ public class MyView {
         RequestDispatcher dispatcher=req.getRequestDispatcher(viewPath);
         dispatcher.forward(req,res);
         }
+
+    public void render(Map<String, Object> model, HttpServletRequest req, HttpServletResponse res)throws ServletException, IOException {
+        modelToRequestAttribute(model,req);
+        RequestDispatcher dispatcher=req.getRequestDispatcher(viewPath);
+        dispatcher.forward(req,res);
+    }
+    private void modelToRequestAttribute(Map<String,Object> model, HttpServletRequest req){
+        model.forEach((key,value)->req.setAttribute(key,value)); //자체 모델의 값을 전부 넣는다.
+    }
 }
 
 /**
---
+++ src/main/java/hello/servlet/web/frontcontroller/v3/controller/MemberFormControllerV3.java
@@ -0,0 +1,19 @@
+package hello.servlet.web.frontcontroller.v3.controller;
+
+import hello.servlet.web.frontcontroller.ModelView;
+import hello.servlet.web.frontcontroller.v3.ControllerV3;
+
+import java.util.Map;
+
+public class MemberFormControllerV3 implements ControllerV3 {
+
+    @Override
+    public ModelView process(Map<String, String> paramMap) {
+        return new ModelView("new-form");
+    }
+}
+
+/**
+ * 컨트롤러에서 지정하는 뷰 이름에 중복이 있는 것을 확인할 수 있다.
+ * 컨트롤러는 뷰의 논리 이름을 반환하고, 실제 물리 위치의 이름은 프론트 컨트롤러에서 처리하도록 단순화.
+ */
