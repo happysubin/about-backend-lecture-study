f7c13286e57b6295b3f10553ea04eaf078554b36
happysubin <asb0711@gmail.com>
2022-01-12 00:03:19 +0900

    Template layout 1

--- src/main/java/hello/thymeleaf/basic/TemplateController.java
+++ src/main/java/hello/thymeleaf/basic/TemplateController.java
@@ -7,8 +7,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/template")
 public class TemplateController {
+
     @GetMapping("/fragment")
     public String template() {
         return "template/fragment/fragmentMain";
+    }
+
+    @GetMapping("/layout")
+    public String layout() {
+        return "template/layout/layoutMain";
     }
 }
