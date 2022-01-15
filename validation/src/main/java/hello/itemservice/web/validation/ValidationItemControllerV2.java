40ad84d54796149981d3d480ed11bdb321e12096
happysubin <asb0711@gmail.com>
2022-01-15 14:32:12 +0900

    BindingResult2, FieldError, ObjectError

--- validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -46,7 +46,7 @@ public class ValidationItemControllerV2 {
         return "validation/v2/addForm";
     }
 
-    @PostMapping("/add")
+    //@PostMapping("/add")
     public String addItemV1(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
         //bindingResult에는  Item에 바인딩이 된 결과가 담긴다. 이게 이전에 errors역할을 해준다.
         //Map<String,String> errors= new HashMap<>();
@@ -90,6 +90,61 @@ public class ValidationItemControllerV2 {
         return "redirect:/validation/v2/items/{itemId}";
     }
 
+    @PostMapping("/add")
+    public String addItemV2(@ModelAttribute Item item,BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model){
+
+        if(!StringUtils.hasText(item.getItemName())){
+            bindingResult.addError(new FieldError("item", "itemName",
+                    item.getItemName(), false, null, null, "상품 이름은 필수입니다."));
+            //여기서 rejectedValue 가 바로 오류 발생시 사용자 입력 값을 저장하는 필드다.
+        }
+
+        /** FieldError 매개변수들들
+        * objectName : 오류가 발생한 객체 이름
+         * field : 오류 필드
+         * rejectedValue : 사용자가 입력한 값(거절된 값)
+         * bindingFailure : 타입 오류 같은 바인딩 실패인지, 검증 실패인지 구분 값 (데이터는 일단 잘 들어와서 우리는 false)
+         * codes : 메시지 코드
+         * arguments : 메시지에서 사용하는 인자
+         * defaultMessage : 기본 오류 메시지
+         */
+
+        if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() >
+                1000000) {
+            bindingResult.addError(new FieldError("item", "price", item.getPrice(),
+                    false, null, null, "가격은 1,000 ~ 1,000,000 까지 허용합니다."));
+        }
+        if (item.getQuantity() == null || item.getQuantity() > 10000) {
+            bindingResult.addError(new FieldError("item", "quantity",
+                    item.getQuantity(), false, null, null, "수량은 최대 9,999 까지 허용합니다."));
+        }
+        //특정 필드 예외가 아닌 전체 예외
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if (resultPrice < 10000) {
+                bindingResult.addError(new ObjectError("item", null, null,
+                        "가격 * 수량의 합은 10,000원 이상이어야 합니다. 현재 값 = " + resultPrice));
+            }
+        }
+        if (bindingResult.hasErrors()) {
+            log.info("errors={}", bindingResult);
+            return "validation/v2/addForm";
+        }
+
+        //성공로직
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    } //이제 form에 기존에 넣었던 값들이 유지된다. 이전 V1에서는 안남았었다.
+
+    //스프링의 바인딩 오류 처리
+    //타입 오류로 바인딩에 실패하면 스프링은 FieldError 를 생성하면서 사용자가 입력한 값을 넣어둔다.
+    //그리고 해당 오류를 BindingResult 에 담아서 컨트롤러를 호출한다. 따라서 타입 오류 같은 바인딩
+    //실패시에도 사용자의 오류 메시지를 정상 출력할 수 있다.
+
+
+
     @GetMapping("/{itemId}/edit")
     public String editForm(@PathVariable Long itemId, Model model) {
         Item item = itemRepository.findById(itemId);
@@ -104,3 +159,21 @@ public class ValidationItemControllerV2 {
     }
 
 }
+
+/**BindingResult
+ * 스프링이 제공하는 검증 오류를 보관하는 객체이다. 검증 오류가 발생하면 여기에 보관하면 된다.
+ * BindingResult 가 있으면 @ModelAttribute 에 데이터 바인딩 시 오류가 발생해도 컨트롤러가 호출된다!
+ *
+ * BindingResult 가 없으면 400 오류가 발생하면서 컨트롤러가 호출되지 않고, 오류 페이지로 이동한다.
+ * BindingResult 가 있으면 오류 정보( FieldError )를 BindingResult 에 담아서 컨트롤러를 정상 호출한다.
+ *
+ * BindingResult에 검증 오류를 적용하는 3가지 방법
+ *
+ * 1. @ModelAttribute 의 객체에 타입 오류 등으로 바인딩이 실패하는 경우 스프링이 FieldError 생성해서 BindingResult 에 넣어준다. 즉 알아서 해준다,
+ * 2. 개발자가 직접 넣어준다.
+ * 3. Validator 사용
+ *
+ * BindingResult 는 검증할 대상 바로 다음에 와야한다. 순서가 중요하다. 예를 들어서 @ModelAttribute
+ * Item item , 바로 다음에 BindingResult 가 와야 한다.
+ * BindingResult 는 Model에 자동으로 포함된다
+ */
