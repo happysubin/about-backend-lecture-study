d8e318ca3bfa91bc100527f635c76b3ad20e9c49
happysubin <asb0711@gmail.com>
2022-01-17 18:31:38 +0900

    Login 처리하기 - Servlet Http session part 2

--- login/src/main/java/hello/login/web/HomeController.java
+++ login/src/main/java/hello/login/web/HomeController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -58,7 +59,7 @@ public class HomeController {
 
     }
 
-    @GetMapping("/")
+    //@GetMapping("/")
     public String homeLoginV3(HttpServletRequest request ,Model model){
 
 
@@ -75,6 +76,22 @@ public class HomeController {
 
         return "loginHome";
 
+    }
+
+
+    @GetMapping("/")
+    public String homeLoginV3(
+            @SessionAttribute(name=SessionConst.LOGIN_MEMBER,required = false) Member loginMember ,
+            Model model){ //이미 로그인 된 사용자를 찾을 때는 다음과 같이 사용하면 된다. 참고로 이 기능은 세션을 생성하지 않는다
+
+        //세션에 회원 데이터가 없으면 home
+        if (loginMember == null) {
+            return "home";
+        }
+
+        //세션이 유지되면 로그인으로 이동
+        model.addAttribute("member", loginMember);
+        return "loginHome";
     }
 
 }
