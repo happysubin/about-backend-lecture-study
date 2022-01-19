bfa672cbccc174e43d504c202bb10cf07328629d
happysubin <asb0711@gmail.com>
2022-01-19 17:26:25 +0900

    Servlet exception 처리 - Interceptor

--- exception/src/main/java/hello/exception/WebConfig.java
+++ exception/src/main/java/hello/exception/WebConfig.java
@@ -1,9 +1,12 @@
 package hello.exception;
 
 import hello.exception.filter.LogFilter;
+import hello.exception.interceptor.LogInterceptor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.DispatcherType;
@@ -13,7 +16,17 @@ import javax.servlet.Filter;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Bean
+    @Override
+    public void addInterceptors(InterceptorRegistry registry){
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**","*.ico","/error","/error-page/**");//오류 페이지 경로
+    }
+    //여기에서 /error-page/** 를 제거하면 error-page/500 같은 내부 호출의 경우에도 인터셉터가 호출된다.
+
+
+    //@Bean
     public FilterRegistrationBean logFilter(){
         FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
         filterFilterRegistrationBean.setFilter(new LogFilter());
@@ -25,3 +38,16 @@ public class WebConfig implements WebMvcConfigurer {
         return filterFilterRegistrationBean;
     }
 }
+
+
+/**
+ * /error-ex 오류 요청
+ * 필터는 DispatchType 으로 중복 호출 제거 ( dispatchType=REQUEST )
+ * 인터셉터는 경로 정보로 중복 호출 제거( excludePathPatterns("/error-page/**") )
+ * 1. WAS(/error-ex, dispatchType=REQUEST) -> 필터 -> 서블릿 -> 인터셉터 -> 컨트롤러
+ * 2. WAS(여기까지 전파) <- 필터 <- 서블릿 <- 인터셉터 <- 컨트롤러(예외발생)
+ * 3. WAS 오류 페이지 확인
+ * 4. WAS(/error-page/500, dispatchType=ERROR) -> 필터(x) -> 서블릿 -> 인터셉터(x) ->
+ * 컨트롤러(/error-page/500) -> View
+ *
+ */
