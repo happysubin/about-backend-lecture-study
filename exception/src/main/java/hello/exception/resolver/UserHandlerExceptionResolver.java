76722974ecaf8d77bdf45c370ae2dc6bd6a9397d
happysubin <asb0711@gmail.com>
2022-01-20 17:20:50 +0900

    ResponseStatusExceptionResolver
    스프링이 제공하는 첫 번째 리졸버

--- exception/src/main/java/hello/exception/resolver/UserHandlerExceptionResolver.java
+++ exception/src/main/java/hello/exception/resolver/UserHandlerExceptionResolver.java
@@ -13,7 +13,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Slf4j
-public class UserHandlerExceptionResolver implements HandlerExceptionResolver {
+public class
+UserHandlerExceptionResolver implements HandlerExceptionResolver {
 
     private final ObjectMapper objectMapper = new ObjectMapper();
 
