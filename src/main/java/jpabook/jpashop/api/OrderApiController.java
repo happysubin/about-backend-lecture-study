d1a61f8308ac65665a60d6cfab369fe0677ab3ce
happysubin <asb0711@gmail.com>
2022-03-04 00:20:43 +0900

    컬렉션 조회 최적화 시작
    역시 엔티티를 사용. 그러나 절대 사용하지 말 것

---
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -0,0 +1,42 @@
+package jpabook.jpashop.api;
+
+
+//Order 기준 컬렉션인 OrderItem과 Item 필요하다. 컬렉션 조회 최적화에 대한 공부 시작!
+
+import jpabook.jpashop.domain.*;
+import jpabook.jpashop.repository.OrderRepository;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.aspectj.weaver.ast.Or;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderApiController {
+
+    private final OrderRepository orderRepository;
+
+
+    /**
+     * V1. 엔티티 직접 노출
+     * - Hibernate5Module 모듈 등록, LAZY=null 처리 * - 양방향 관계 문제 발생 -> @JsonIgnore
+     */
+    @GetMapping("/api/v1/orders")
+    public List<Order> ordersV1(){
+        List<Order> orders=orderRepository.findAllByString(new OrderSearch());
+        for (Order order : orders) {
+            order.getMember().getName(); //Lazy 강제 초기화
+            order.getDelivery().getAddress(); //Lazy 강제 초기환
+            List<OrderItem> orderItems = order.getOrderItems();
+            orderItems.stream().forEach(o -> o.getItem().getName()); //Lazy 강제
+        }
+        return orders;
+    }
+
+    
+}
