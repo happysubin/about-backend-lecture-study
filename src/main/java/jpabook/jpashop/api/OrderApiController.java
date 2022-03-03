c924ecf780d381c8d0d4efb87862bb84e61a7822
happysubin <asb0711@gmail.com>
2022-03-04 00:32:04 +0900

    V2 : 엔티티를 DTO로 변환
    조회 엔티티와 연관 관계를 가지는 컬렉션의 엔티티도 DTO로 변환

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -38,5 +38,55 @@ public class OrderApiController {
         return orders;
     }
 
-    
+    @GetMapping("/api/v2/orders")
+    public List<OrderDto> ordersV2(){
+        List<Order> orders= orderRepository.findAllByString(new OrderSearch());
+        List<OrderDto> collect = orders.stream()
+                .map(o -> new OrderDto(o))
+                .collect(Collectors.toList());
+        return collect;
+    }
+
+    /**
+     * 지연 로딩으로 너무 많은 SQL 실행 SQL 실행 수
+     *  order 1번
+     *   member , address N번(order 조회 수 만큼)
+     *   orderItem N번(order 조회 수 만큼)
+     * item N번(orderItem 조회 수 만큼)
+     */
+
+
+    @Data
+    static class OrderDto {
+
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate; //주문시간 private OrderStatus orderStatus;
+        private Address address;
+        private OrderStatus orderStatus;
+        private List<OrderItemDto> orderItems;
+
+        public OrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName();
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress();
+            orderItems = order.getOrderItems().stream()
+                    .map(orderItem -> new OrderItemDto(orderItem))
+                    .collect(Collectors.toList());
+        }
+    }
+
+    @Data
+    static class OrderItemDto {
+        private String itemName;//상품 명
+        private int orderPrice; //주문 가격
+        private int count; //주문 수량
+        public OrderItemDto(OrderItem orderItem) {
+            itemName = orderItem.getItem().getName();
+            orderPrice = orderItem.getOrderPrice();
+            count = orderItem.getCount();
+        }
+    }
 }
