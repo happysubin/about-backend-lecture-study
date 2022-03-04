8dae6904b636e266c0f2bd07653e7dbc884ffeeb
happysubin <asb0711@gmail.com>
2022-03-04 18:51:56 +0900

    참고 V6 : JPA에서 DTO로 직접 조회, 플랫 데이터 최적화
    그냥 가볍게 이렇게 복잡하고 더럽구나...라는 걸 인지하자

--- src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
+++ src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
@@ -104,5 +104,17 @@ public class OrderQueryRepository {
      * MAP을 사용해서 매칭 성능 향상(O(1))
      */
 
+
+    public List<OrderFlatDto> findAllByDto_flat() {
+        return em.createQuery(
+                "select new " +
+                        "jpabook.jpashop.repository.order.query.OrderFlatDto(o.id, m.name, o.orderDate, o.status, d.address, i.name, oi.orderPrice, oi.count)" +
+                        " from Order o" +
+                        " join o.member m" +
+                        " join o.delivery d" +
+                        " join o.orderItems oi" +
+                        " join oi.item i", OrderFlatDto.class).getResultList();
+
+    }
 }
 
