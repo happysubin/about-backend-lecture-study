3a9da902a1cb3bee53df31771a1feb12d5ae154b
happysubin <asb0711@gmail.com>
2022-03-17 23:21:19 +0900

    first commit

---
+++ src/main/java/springapi/api/HelloController.java
@@ -0,0 +1,26 @@
+package springapi.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping(path="/hello-world")
+    public String hello(){
+        return "Hello World";
+    }
+
+    @GetMapping("/hello-world2")
+    public HelloWorldBean helloWorldBean(){
+        return new HelloWorldBean("hello world bean"); //REST controller을 쓰면 json으로 반환해준다.
+    }
+
+    @GetMapping("/hello-world/path-variable/{id}")
+    public HelloWorldBean pathVariable(@PathVariable String id){
+        System.out.println("id = " + id);
+        return new HelloWorldBean("hello world bean "+id); //REST controller을 쓰면 json으로 반환해준다.
+    }
+
+}
---
+++ src/main/java/springapi/api/HelloWorldBean.java
@@ -0,0 +1,13 @@
+package springapi.api;
+
+import lombok.Data;
+
+@Data
+public class HelloWorldBean {
+
+    private String message;
+
+    public HelloWorldBean(String message) {
+        this.message = message;
+    }
+}
