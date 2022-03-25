4c2c7c0109ea71db0fea4f556db0eab8a7a63ee3
happysubin <asb0711@gmail.com>
2022-03-26 02:57:43 +0900

    Response data Filtering

--- src/main/java/springapi/api/domain/User.java
+++ src/main/java/springapi/api/domain/User.java
@@ -1,5 +1,9 @@
 package springapi.api.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,11 +12,11 @@ import javax.validation.constraints.Size;
 import java.util.Date;
 
 
-@Getter
-@Setter
+@Data
+@AllArgsConstructor
+@JsonIgnoreProperties(value = {"password"}) //password만 제외딘다. 클래스 상단에서 사용
 public class User {
     private Long id;
-
 
     @Size(min=2,message = "Name는 2글자 이상 입력하세요") //한 글자만 넣으면 상태코드 400이 리턴증
     private String name;
@@ -20,9 +24,12 @@ public class User {
     @Past// 과거 데이터만 사용 가능
     private Date joinDate;
 
-    public User(Long id, String name, Date joinDate) {
-        this.id = id;
-        this.name = name;
-        this.joinDate = joinDate;
-    }
+    //@JsonIgnore //json에서 무시됨. 즉 클라이언트에게 json 문서를 줄 때 해당 필드가 안보인다.
+    private String password;
+    //@JsonIgnore
+    private String ssn;
+
+
+
+
 }
