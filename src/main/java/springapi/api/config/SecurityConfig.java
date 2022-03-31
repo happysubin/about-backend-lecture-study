60eb7bf1303b2fb2480a50dde78e2967483b5231
happysubin <asb0711@gmail.com>
2022-03-31 16:43:57 +0900

    jpa part start

--- src/main/java/springapi/api/config/SecurityConfig.java
+++ src/main/java/springapi/api/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package springapi.api.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
@@ -16,6 +17,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     .password("{noop}1234") //인코딩을 없이 사용하기 위해 옵션 부여 {noop}
                     .roles("USER");
 
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/h2-console/**").permitAll();
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
     }
 }
 //yml이 아니라 이 클래스를 사용해 설정
