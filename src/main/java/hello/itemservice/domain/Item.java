2142aa5284b202df5e2654468bbd70c3327c5ccf
happysubin <asb0711@gmail.com>
2022-06-22 17:27:28 +0900

    Pure Jpa 적용

--- src/main/java/hello/itemservice/domain/Item.java
+++ src/main/java/hello/itemservice/domain/Item.java
@@ -1,18 +1,26 @@
 package hello.itemservice.domain;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.*;
 
 @Data
+@Entity
+@NoArgsConstructor //JPA는 public, protected 생성자가 필수. 동적 프록시 기술을 사용하기 위해
 public class Item {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "item_name", length = 10) //캐멀 케이스와 사실 스네이크 케이스를 저절로 바꿔준다
     private String itemName;
     private Integer price;
     private Integer quantity;
 
-    public Item() {
-    }
 
     public Item(String itemName, Integer price, Integer quantity) {
         this.itemName = itemName;
