9c4859abcd3e749bcc887a4400e95a7e9e8befa1
happysubin <asb0711@gmail.com>
2022-06-26 19:04:23 +0900

    로그 추적기 v2 파라미터 동기화 개발

--- src/main/java/hello/advanced/trace/hellotrace/HelloTraceV2.java
+++ src/main/java/hello/advanced/trace/hellotrace/HelloTraceV2.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
-public class HelloTraceV1 {
+public class HelloTraceV2 {
     private static final String START_PREFIX = "-->";
     private static final String COMPLETE_PREFIX = "<--";
     private static final String EX_PREFIX = "<X-";
@@ -20,9 +20,18 @@ public class HelloTraceV1 {
         return new TraceStatus(traceId, startTimeMs, message);
     }
 
+    public TraceStatus beginSync(TraceId beforeTraceId, String message) {
+        TraceId nextId = beforeTraceId.createNextId();
+        Long startTimeMs = System.currentTimeMillis();
+        log.info("[" + nextId.getId() + "] " + addSpace(START_PREFIX, nextId.getLevel()) + message);
+
+        return new TraceStatus(nextId, startTimeMs, message);
+    }
+
     public void end(TraceStatus status) {
         complete(status, null);
     }
+
     public void exception(TraceStatus status, Exception e) {
         complete(status, e);
     }
