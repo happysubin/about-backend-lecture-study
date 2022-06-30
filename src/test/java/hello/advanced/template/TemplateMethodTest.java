926e0832d9ac8539b993743e9e370f93b1719cb2
happysubin <asb0711@gmail.com>
2022-06-30 12:34:03 +0900

    feat: 익명 내부 클래스를 사용한 탬플릿 메서드 패턴 사용

--- src/test/java/hello/advanced/template/TemplateMethodTest.java
+++ src/test/java/hello/advanced/template/TemplateMethodTest.java
@@ -45,5 +45,25 @@ public class TemplateMethodTest {
         AbstractTemplate template2 = new SubClassLogic2();
         template2.execute();
     }
+
+    //템플릿 메서드 패턴은 SubClassLogic1 , SubClassLogic2 처럼 클래스를 계속 만들어야 하는 단점이 있다. 이것을 해결해야 함.
+
+    @Test
+    void templateMethodV2() {
+        AbstractTemplate template1 = new AbstractTemplate() {
+            @Override
+            protected void call() { log.info("비즈니스 로직1 실행");
+            }
+        };
+        log.info("클래스 이름1={}", template1.getClass()); template1.execute();
+
+        AbstractTemplate template2 = new AbstractTemplate() {
+            @Override
+            protected void call() { log.info("비즈니스 로직1 실행");
+            }
+        };
+        log.info("클래스 이름2={}", template2.getClass());
+        template2.execute();
+    }
 }
 
