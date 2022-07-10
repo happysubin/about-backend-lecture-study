340a3f07c899ccc375bd00f27c89df7feb446d3a
happysubin <asb0711@gmail.com>
2022-07-10 13:09:24 +0900

    Feat: 예제 작성 및 기초 스프링 AOP 구현

---
+++ src/test/java/hello/aop/AopTest.java
@@ -0,0 +1,42 @@
+package hello.aop;
+
+import hello.aop.order.OrderRepository;
+import hello.aop.order.OrderService;
+import hello.aop.order.aop.AspectV1;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.annotation.Aspect;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.support.AopUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+import static org.assertj.core.api.AssertionsForClassTypes.*;
+
+@Import(AspectV1.class) //@Import 주로 설정 파일을 추가할 때 사용( @Configuration )
+@SpringBootTest
+public class AopTest {
+
+    @Autowired
+    OrderService orderService;
+
+    @Autowired
+    OrderRepository orderRepository;
+
+    @Test
+    void aopInfo() {
+        System.out.println("isAopProxy, orderService=" + AopUtils.isAopProxy(orderService));
+        System.out.println("isAopProxy, orderRepository=" + AopUtils.isAopProxy(orderRepository));
+    }
+
+    @Test
+    void success() {
+        orderService.orderItem("itemA");
+    }
+
+    @Test
+    void exception() {
+        assertThatThrownBy(() -> orderService.orderItem("ex"))
+                .isInstanceOf(IllegalStateException.class);
+    }
+}
