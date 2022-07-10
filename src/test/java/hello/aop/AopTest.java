3598b13542088702402184e9fc9267e560e943a0
happysubin <asb0711@gmail.com>
2022-07-10 13:52:15 +0900

    Feat: 어드바이스 순서 보장:클래스 단위로 적용

--- src/test/java/hello/aop/AopTest.java
+++ src/test/java/hello/aop/AopTest.java
@@ -2,10 +2,7 @@ package hello.aop;
 
 import hello.aop.order.OrderRepository;
 import hello.aop.order.OrderService;
-import hello.aop.order.aop.AspectV1;
-import hello.aop.order.aop.AspectV2;
-import hello.aop.order.aop.AspectV3;
-import hello.aop.order.aop.AspectV4Pointcut;
+import hello.aop.order.aop.*;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.annotation.Aspect;
 import org.junit.jupiter.api.Test;
@@ -19,7 +16,8 @@ import static org.assertj.core.api.AssertionsForClassTypes.*;
 //@Import(AspectV1.class) //@Import 주로 설정 파일을 추가할 때 사용( @Configuration )
 //@Import(AspectV2.class)
 //@Import(AspectV3.class)
-@Import(AspectV4Pointcut.class)
+//@Import(AspectV4Pointcut.class)
+@Import({AspectV5Order.LogAspect.class, AspectV5Order.TxAspect.class})
 @SpringBootTest
 public class AopTest {
 
