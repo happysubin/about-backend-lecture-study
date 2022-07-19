a3ed47ab0d0d33abc9c0aa0bc97a4ca20be49465
happysubin <asb0711@gmail.com>
2022-07-19 16:27:39 +0900

    Feat: 초기 세팅 및 확인

--- src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -1,13 +1,41 @@
 package study.querydsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
+
+import static org.assertj.core.api.Assertions.*;
+
+@Commit
+@Transactional
 @SpringBootTest
 class QuerydslApplicationTests {
 
+	@Autowired
+	EntityManager em;
+
 	@Test
 	void contextLoads() {
+		Hello hello = new Hello();
+		em.persist(hello);
+
+		JPAQueryFactory queryFactory = new JPAQueryFactory(em);
+		QHello qHello = new QHello("h");
+
+
+
+		Hello result = queryFactory.
+				selectFrom(qHello) //쿼리와 관련된 것은 전부 Q 타입을 넣는다.
+				.fetchOne();
+
+		assertThat(result).isEqualTo(hello);
+		assertThat(result.getId()).isEqualTo(hello.getId());
 	}
 
 }
