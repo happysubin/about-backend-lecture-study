641cb13f350f6db1321e955a7b7ab1cdf142b67a
happysubin <asb0711@gmail.com>
2022-07-31 00:57:39 +0900

    Feat: fetch join 학습

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -8,12 +8,15 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
 import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.entity.Member;
 import study.querydsl.entity.QMember;
 import study.querydsl.entity.Team;
 
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceUnit;
 
 import java.util.List;
 
@@ -291,5 +294,38 @@ public class QuerydslBasicTest {
      * 일반조인: leftJoin(member.team, team) on조인: from(member).leftJoin(team).on(xxx)
      */
 
+    @PersistenceUnit
+    EntityManagerFactory emf;
+
+    @Test
+    void noFetchJoin(){
+        em.flush();
+        em.clear();
+
+        //현재 지연로딩으로 멤버만 select 된다.
+        Member findMember = query.selectFrom(member)
+                .where(member.username.eq("member1"))
+                .fetchOne();
+
+        boolean loaded = emf.getPersistenceUnitUtil().isLoaded(findMember.getTeam());
+        assertThat(loaded).as("페치 조인 미적용").isFalse();
+    }
+
+    @Test
+    void useFetchJoin(){
+        em.flush();
+        em.clear();
+
+        //현재 지연로딩으로 멤버만 select 된다.
+        Member findMember = query.selectFrom(member)
+                .join(member.team, team).fetchJoin()
+                .where(member.username.eq("member1"))
+                .fetchOne();
+
+        boolean loaded = emf.getPersistenceUnitUtil().isLoaded(findMember.getTeam());
+        assertThat(loaded).as("페치 조인 적용").isTrue();
+    }
+
+    //join(), leftJoin() 등 조인 기능 뒤에 fetchJoin() 이라고 추가하면 된다.
 
 }
