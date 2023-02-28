9fed3d8df61b06006b38bd7f93a209e05a66761b
happysubin <asb0711@gmail.com>
2023-02-28 22:15:49 +0900

    내장 톰캣3 - 스프링

--- embed/src/main/java/hello/embed/EmbedTomcatServletMain.java
+++ embed/src/main/java/hello/embed/EmbedTomcatServletMain.java
@@ -1,10 +1,13 @@
 package hello.embed;
 
 import hello.servlet.HelloServlet;
+import hello.spring.HelloConfig;
 import org.apache.catalina.Context;
 import org.apache.catalina.LifecycleException;
 import org.apache.catalina.connector.Connector;
 import org.apache.catalina.startup.Tomcat;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 public class EmbedTomcatServletMain {
 
@@ -17,10 +20,16 @@ public class EmbedTomcatServletMain {
         connector.setPort(8080);
         tomcat.setConnector(connector);
 
-        //서블릿 등록
+
+        //스프링 컨테이너 생성
+        AnnotationConfigWebApplicationContext appContext = new AnnotationConfigWebApplicationContext();
+        appContext.register(HelloConfig.class); //스프링 MVC 디스패처 서블릿 생성, 스프링 컨테이너 연결
+        DispatcherServlet dispatcher = new DispatcherServlet(appContext);
+
+        //디스패처 서블릿 등록
         Context context = tomcat.addContext("", "/");
-        tomcat.addServlet("", "helloServlet", new HelloServlet());
-        context.addServletMappingDecoded("/hello-servlet", "helloServlet");
+        tomcat.addServlet("", "dispatcher", dispatcher);
+        context.addServletMappingDecoded("/", "dispatcher");
 
         tomcat.start();
     }
