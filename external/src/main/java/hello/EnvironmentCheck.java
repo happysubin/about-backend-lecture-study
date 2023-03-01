b6e06be43d8c20d6a5388b0304ffae4829657262
happysubin <asb0711@gmail.com>
2023-03-02 06:55:44 +0900

    설정 데이터 - 내부 파일 분리

--- external/src/main/java/hello/EnvironmentCheck.java
+++ external/src/main/java/hello/EnvironmentCheck.java
@@ -1,2 +1,29 @@
-package hello;public class EnvironmentCheck {
+package hello;
+
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class EnvironmentCheck {
+
+    private final Environment env;
+
+    public EnvironmentCheck(Environment environment) {
+        this.env = environment;
+    }
+
+    @PostConstruct
+    public void init(){
+        log.info(env.getClass().toString());
+
+        String url = env.getProperty("url");
+        String username = env.getProperty("username");
+        String password = env.getProperty("password");
+        log.info("env url={}", url);
+        log.info("env username={}", username);
+        log.info("env password={}", password);
+    }
 }
