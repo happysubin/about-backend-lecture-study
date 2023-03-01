cb50ee732fd4baed2efff5f235021c64be53b0af
happysubin <asb0711@gmail.com>
2023-03-01 19:13:12 +0900

    자동 라이브러리 사용하기

--- project-v2/src/main/java/hello/HelloController.java
+++ project-v2/src/main/java/hello/HelloController.java
@@ -1,2 +1,13 @@
-package hello;public class HelloController {
+package hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
 }
