cea7e6e7540803229d035cc19ada885dd6d23d6c
happysubin <asb0711@gmail.com>
2023-03-02 18:19:15 +0900

    @ConfigurationProperties 시작

--- external-read/src/main/java/hello/ExternalReadApplication.java
+++ external-read/src/main/java/hello/ExternalReadApplication.java
@@ -1,17 +1,20 @@
 package hello;
 
+import hello.config.MyDataSourceConfigV1;
 import hello.config.MyDataSourceEnvConfig;
 import hello.config.MyDataSourceValueConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Import;
 
-@Import(MyDataSourceValueConfig.class)
+//@Import(MyDataSourceValueConfig.class)
+@Import(MyDataSourceConfigV1.class)
 @SpringBootApplication(scanBasePackages = "hello.datasource")
+@ConfigurationPropertiesScan({ "hello.datasource" }) //컴포넌트 스캔 자동 등록?
 public class ExternalReadApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ExternalReadApplication.class, args);
     }
-
 }
