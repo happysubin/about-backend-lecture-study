c8afa3a43619d7a1f66ee6cf987022ec366c60b9
happysubin <asb0711@gmail.com>
2023-03-03 19:49:32 +0900

    actuator 프로젝트 설정

--- external-read/src/main/java/hello/ExternalReadApplication.java
+++ external-read/src/main/java/hello/ExternalReadApplication.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Import;
 
 //@Import(MyDataSourceValueConfig.class)
 @Import(MyDataSourceConfigV3.class)
-@SpringBootApplication(scanBasePackages = "hello.datasource")
+@SpringBootApplication(scanBasePackages = {"hello.datasource", "hello.pay"})
 //@ConfigurationPropertiesScan({ "hello.datasource" }) //컴포넌트 스캔 자동 등록?
 public class ExternalReadApplication {
 
