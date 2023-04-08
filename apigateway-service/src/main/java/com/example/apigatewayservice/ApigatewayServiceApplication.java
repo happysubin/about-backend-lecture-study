1e056a2b2a6e1e34518208e984bd780c24f9a84a
happysubin <asb0711@gmail.com>
2023-04-08 22:33:10 +0900

    Spring Cloud Gateway - Load Balancer 적용

--- apigateway-service/src/main/java/com/example/apigatewayservice/ApigatewayServiceApplication.java
+++ apigateway-service/src/main/java/com/example/apigatewayservice/ApigatewayServiceApplication.java
@@ -2,7 +2,9 @@ package com.example.apigatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ApigatewayServiceApplication {
 
--- userservice/src/main/java/com/example/userservice/UserController.java
+++ userservice/src/main/java/com/example/userservice/UserController.java
@@ -5,9 +5,13 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 @RequestMapping("/first-service")
 public class UserController {
+
+
 
     @GetMapping("/hello")
     public String hello(){
@@ -21,7 +25,7 @@ public class UserController {
     }
 
     @GetMapping("/check")
-    public String check (){
-        return "check";
+    public String check (HttpServletRequest request){
+        return "check " + request.getServerPort();
     }
 }
