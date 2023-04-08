b2163cbd0a7b3377307ef1313ac8a5b83d74a26e
happysubin <asb0711@gmail.com>
2023-04-08 21:30:22 +0900

    Spring Cloud Gateway에서 자바 설정을 통한 HTTP 헤더 다루기

--- userservice/src/main/java/com/example/userservice/UserController.java
+++ userservice/src/main/java/com/example/userservice/UserController.java
@@ -1,13 +1,22 @@
 package com.example.userservice;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/first-service")
 public class UserController {
 
-    @GetMapping("/first-service/hello")
+    @GetMapping("/hello")
     public String hello(){
         return "hello";
+    }
+
+    @GetMapping("/message")
+    public String message (@RequestHeader("first-request") String header){
+        System.out.println("header = " + header);
+        return header;
     }
 }
