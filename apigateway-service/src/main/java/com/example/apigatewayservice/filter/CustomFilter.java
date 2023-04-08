7184808c0dcec8e5dd9dc03b91d0d7e8842a547a
happysubin <asb0711@gmail.com>
2023-04-08 22:09:06 +0900

    Spring Cloud Gateway  LoggingFilter 등록

--- apigateway-service/src/main/java/com/example/apigatewayservice/filter/CustomFilter.java
+++ apigateway-service/src/main/java/com/example/apigatewayservice/filter/CustomFilter.java
@@ -1,4 +1,4 @@
-package com.example.apigatewayservice;
+package com.example.apigatewayservice.filter;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
--- apigateway-service/src/main/java/com/example/apigatewayservice/filter/FilterConfig.java
+++ apigateway-service/src/main/java/com/example/apigatewayservice/filter/FilterConfig.java
@@ -1,4 +1,4 @@
-package com.example.apigatewayservice;
+package com.example.apigatewayservice.filter;
 
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
