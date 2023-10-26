35e01507945c12040467017b5f8ef69a54ef199f
happysubin <asb0711@gmail.com>
2023-10-26 21:10:31 +0900

    Flow 파트 Transition까지 학습

--- spring-batch/springbatchlecture/src/main/java/io/springbatch/springbatchlecture/SpringBatchLectureApplication.java
+++ spring-batch/springbatchlecture/src/main/java/io/springbatch/springbatchlecture/SpringBatchLectureApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
 
-@ComponentScan("io.springbatch.springbatchlecture.part5_step.jobstep")
+@ComponentScan("io.springbatch.springbatchlecture.part6_flow")
 @EnableBatchProcessing
 @SpringBootApplication
 public class SpringBatchLectureApplication {
