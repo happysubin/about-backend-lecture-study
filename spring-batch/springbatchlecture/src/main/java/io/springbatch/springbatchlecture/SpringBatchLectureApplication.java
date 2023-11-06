ebdd640819fd9d3614de0a43f0e7180b8d18e6ba
happysubin <asb0711@gmail.com>
2023-11-07 07:28:06 +0900

    JsonFileItemWriter 학습

--- spring-batch/springbatchlecture/src/main/java/io/springbatch/springbatchlecture/SpringBatchLectureApplication.java
+++ spring-batch/springbatchlecture/src/main/java/io/springbatch/springbatchlecture/SpringBatchLectureApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
 
-@ComponentScan("io.springbatch.springbatchlecture.part9_itemwriter.flatfile")
+@ComponentScan("io.springbatch.springbatchlecture.part9_itemwriter.json")
 @EnableBatchProcessing
 @SpringBootApplication
 public class SpringBatchLectureApplication {
