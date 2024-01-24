17db4b38ba08d9a9595711f85dcbee82d1586865
happysubin <asb0711@gmail.com>
2024-01-24 21:15:56 +0900

    코틀린을 사용한 순수 테스트 작성

---
+++ library-app/src/test/kotlin/com/group/libraryapp/calculator/CalculatorTest.kt
@@ -0,0 +1,20 @@
+package com.group.libraryapp.calculator
+
+import org.junit.jupiter.api.Assertions.*
+
+fun main() {
+    val calculatorTest = CalculatorTest()
+    calculatorTest.addTest()
+}
+
+class CalculatorTest {
+
+    fun addTest() {
+        val calculator = Calculator(5)
+        calculator.add(3)
+
+        if(calculator.number != 8) {
+            throw IllegalStateException()
+        }
+    }
+}
