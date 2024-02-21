51f044488f7a0cb7619143e4ef54d9eccd4e7143
happysubin <asb0711@gmail.com>
2024-02-21 21:51:18 +0900

    BookService를 코틀린으로 리팩토링 & Optional 제거

--- library-app/src/main/kotlin/com/group/libraryapp/domain/user/UserRepository.kt
+++ library-app/src/main/kotlin/com/group/libraryapp/domain/user/UserRepository.kt
@@ -5,5 +5,7 @@ import java.util.*
 
 interface UserRepository : JpaRepository<User, Long> {
 
-    fun findByName(nameP: String): Optional<User>
+    //fun findByName(nameP: String): Optional<User>
+    fun findByName(nameP: String): User? //스프링이 알아서 null을 넣어줌.
+
 }
