4a919a5b01595f822b24a18b944b9cf0bcd8f5b5
happysubin <asb0711@gmail.com>
2024-07-29 08:09:28 +0900

    스프링 시큐리티 버전 6. Section05: 세션 생성 정책

--- security/src/main/kotlin/com/lecture/security/section05/SecurityConfig.kt
+++ security/src/main/kotlin/com/lecture/security/section05/SecurityConfig.kt
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer
 import org.springframework.security.config.annotation.web.configurers.SecurityContextConfigurer
+import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.core.userdetails.User
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
@@ -29,22 +30,26 @@ class SecurityConfig {
     http.sessionManagement { session ->
         session
             .sessionFixation {session.sessionFixation().changeSessionId()} //이게 젤 가성비 굿
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS) //JWT 사용하면 해당 enum 값 사용.
             .invalidSessionUrl("/invalidSessionUrl") // 이미 만료된 세션으로 요청을 하는 사용자를 특정 엔드포인트로 리다이렉션 할 Url 을 지정
             .maximumSessions(1) // 사용자당 최대 세션 수를 제어한다. 기본값은 무제한 세션을 허용한다. 이게 젤 중요!
             .maxSessionsPreventsLogin(false) // true 이면 최대 세션 수 (maximumSessions(int))에 도달했을 때 사용자의 인증을 방지한다
                                             // false(기본 설정)이면 인증하는 사용자에게 접근을 허용하고 기존 사용자의 세션은 만료된다
             .expiredUrl("/expired") // 세션을 만료하고 나서 리다이렉션 할 URL 을 지정한다
-
-        //세션 보호
-    }
-
         //사용자 세션 강제 만료에서 invalidSessionUrl과 expiredUrl이 관련있다.
         //사용자 인증 시도 차단에서는 의미 X.
         // invalidSessionUrl과 expiredUrl 둘 중 우선순위는 invalidSessionUrl이 더 높다
+    }
 
 
     return http.build()
     }
+
+    /**
+     * STATELESS 설정에도 세션이 생성될 수 있다
+     * 스프링 시큐리티에서 CSRF 기능이 활성화 되어있고 CSRF 기능이 수행될 경우 사용자의 세션을 생성해서 CSRF 토큰을 저장하게 된다
+     * 세션은 생성되지만 CSRF 기능을 위해서 사용될 뿐 인증 프로세스의 SecurityContext 영속성에 영향을 미치지는 않는다.
+     */
 
 
 
