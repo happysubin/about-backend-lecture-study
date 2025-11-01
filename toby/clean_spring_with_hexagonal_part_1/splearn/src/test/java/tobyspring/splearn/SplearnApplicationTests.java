69e8f9fb52b9765501d208e5cb2a20829417283d
happysubin <asb0711@gmail.com>
2025-11-01 23:50:19 +0900

    토비의 클린 스프링 - 도메인 모델 패턴과 헥사고날 아키텍처 Part 1 도메인 모델 부분 학습

--- toby/clean_spring_with_hexagonal_part_1/splearn/src/test/java/tobyspring/splearn/SplearnApplicationTests.java
+++ toby/clean_spring_with_hexagonal_part_1/splearn/src/test/java/tobyspring/splearn/SplearnApplicationTests.java
@@ -1,5 +1,6 @@
 package tobyspring.splearn;
 
+import com.fasterxml.jackson.databind.deser.BasicDeserializerFactory;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,7 @@ class SplearnApplicationTests {
 
 	@Test
 	void contextLoads() {
+
 	}
 
 }
